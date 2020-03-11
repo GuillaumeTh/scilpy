4d4d51f0d05a4e79e5f20ec1549eef4536ad2776
Guillaume Theaud <guillaume.theaud@gmail.com>
2020-03-11 15:23:27 -0400

    rm setup_requires

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ opts = dict(name=NAME,
             platforms=PLATFORMS,
             version=VERSION,
             packages=find_packages(),
-            setup_requires=["cython (>=0.29.12)", "numpy (>=1.16.2)"],
             install_requires=REQUIRES,
             scripts=SCRIPTS,
             ext_modules=[
