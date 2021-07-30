2390af2cb2e63fbddeea572583d5eac98ee282c9
Guillaume Theaud <guillaume.theaud@gmail.com>
2021-07-30 11:49:43 -0400

    Add output report

--- scripts/scil_compute_bundle_group_qc.py
+++ scripts/scil_compute_bundle_group_qc.py
@@ -12,6 +12,8 @@ import seaborn as sn
 import pandas
 import numpy as np
 import matplotlib
+import tqdm
+import json
 
 from scilpy.io.streamlines import lazy_streamlines_count
 from scilpy.io.utils import add_json_args
@@ -32,7 +34,7 @@ def main():
     args = parser.parse_args()
 
     stats = {}
-    for i in args.in_tractogram:
+    for i in tqdm.tqdm(args.in_tractogram):
         id = os.path.basename(os.path.dirname(os.path.dirname(i)))
         bundle_name, _ = os.path.splitext(os.path.basename(i))
         bundle_name = bundle_name[len(id+'__'):]
@@ -44,22 +46,89 @@ def main():
 
     test = pandas.DataFrame.from_dict(stats)
     ax = sn.boxplot(data=test)
-    ax = sn.swarmplot(data=test, color=".25")
-    ax.set
+    ax = sn.stripplot(data=test, color=".25")
     ax.set_xticklabels(ax.get_xticklabels(), rotation=30)
+    qc_report = []
+    settings = {"type": "settings", "data": [], "username": "auto_qc", "date": ""}
+    report = {"type": "report", "data": []}
+    report_dict = {}
+    subj = set([])
     for i in test.keys():
         val = test[i].dropna().values
         mean = np.mean(val)
         std = np.std(val)
-        for index, value in test[i].iteritems():
-            if value <= mean + std or value >= mean - std:
-                print(index, i, "Pass")
-            elif value <= mean + 2 * std or value >= mean -2 * std:
-                print(index, i, "Warning")
+        for index, value in test[i].dropna().iteritems():
+            if value == np.nan:
+                continue
+            if value < 50:
+                rating = "Fail"
+                comment = "Under 50 streamlines"
+            elif value <= mean + std or value >= mean - std:
+                rating = "Pass"
+                comment = ""
+            elif value <= (mean + 1.5 * std) or value >= (mean - 1.5 * std):
+                rating = "Warning"
+                comment = "Number of streamlines between mean +- 1.5 std"
             else:
-                print(index, i, "Fail")
+                rating = "Fail"
+                comment = "Number of streamlines under or upper than mean +- 1.5 std"
+            if i in report_dict:
+                report_dict[i][index] = {"status": rating, "comments": comment}
+            else:
+                report_dict[i] = {index: {"status": rating, "comments": comment}}
+            subj.add(index)
+
+    b_name_rating = set([])
+    for i in test.keys():
+        if "_L" in i or "_R" in i:
+            b_name = i.replace("_L", "").replace("_R", "")
+            b_name_rating.add(b_name)
+
+    for b_name in b_name_rating:
+        for sub in subj:
+            if b_name not in report_dict:
+                report_dict[b_name] = {}
+            if sub in report_dict[b_name+"_L"] and sub in report_dict[b_name+"_R"]:
+                left = report_dict[b_name+"_L"][sub]["status"]
+                right = report_dict[b_name+"_R"][sub]["status"]
+
+                if left == "Pass" and right == "Pass":
+                    report_dict[b_name][sub] = report_dict[b_name+"_L"][sub]
+
+                if left == "Warning" and right == "Pass":
+                    report_dict[b_name][sub] = report_dict[b_name+"_L"][sub]
+
+                if left == "Pass" and right == "Warning":
+                    report_dict[b_name][sub] = report_dict[b_name+"_R"][sub]
+
+                if left == "Warning" and right == "Warning":
+                    report_dict[b_name][sub] = report_dict[b_name+"_R"][sub]
+
+                if left == "Fail" or right == "Fail":
+                    if left == "Fail":
+                        report_dict[b_name][sub] = report_dict[b_name+"_L"][sub]
+                    else:
+                        report_dict[b_name][sub] = report_dict[b_name+"_R"][sub]
+            elif sub in report_dict[b_name+"_L"]:
+                report_dict[b_name][sub] = {"status": "Fail", "comments": "Missing left bundle"}
+            elif sub in report_dict[b_name+"_R"]:
+                report_dict[b_name][sub] = {"status": "Fail", "comments": "Missing right bundle"}
+
+        report_dict.pop(b_name + "_L")
+        report_dict.pop(b_name + "_R")
+
+    for i in report_dict:
+        for index in report_dict[i]:
+            rating = report_dict[i][index]["status"]
+            comment = report_dict[i][index]["comments"]
+            report["data"].append({"qc": "imgs", "status": rating, "comments": comment, "filename": index+"__"+i+"_cleaned.png"})
+    qc_report.append(settings)
+    qc_report.append(report)
+    with open('report.json', 'w') as f:
+        json.dump(qc_report, f)
     ax.figure.set_size_inches(18.5, 8.5)
     matplotlib.pyplot.savefig("boxplot.png", dpi=100, orientation='landscape')
+
 
 if __name__ == '__main__':
     main()
